c4e2355a6535a6eb3dc34c36e85e0b29bf59ddf8
Adam Faulconbridge <afaulconbridge@googlemail.com>
2025-04-27 14:27:45 +0100

    better fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -6,9 +6,15 @@ from pytest import fixture
 
 @fixture(name="data_directory")
 def fixture_data_directory() -> Path:
-    return Path(os.path.dirname(os.path.realpath(__file__))) / "data"
+    data_path = Path(os.path.dirname(os.path.realpath(__file__))) / "data"
+    assert data_path.exists()
+    assert data_path.is_dir()
+    return data_path
 
 
 @fixture(name="video_path")
 def fixture_video_path(data_directory: Path) -> Path:
-    return data_directory / "test.mp4"
+    video_path = data_directory / "test.mp4"
+    assert video_path.exists()
+    assert video_path.is_file()
+    return video_path
