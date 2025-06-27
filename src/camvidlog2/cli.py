631b8d7b85800815b7902589cbbece4f7be77077
Adam Faulconbridge <afaulconbridge@googlemail.com>
2025-06-27 18:02:50 +0100

    precommit fixes

--- src/camvidlog2/cli.py
+++ src/camvidlog2/cli.py
@@ -1,6 +1,6 @@
-from camvidlog2.bioclip.cli import app as bioclip_app
-
 import typer
+
+from camvidlog2.bioclip.cli import app as bioclip_app
 
 app = typer.Typer()
 app.add_typer(bioclip_app, name="bioclip")
