1fe517cf2d9fb78bad8b55f16360c4a9c39892ea
Adam Faulconbridge <afaulconbridge@googlemail.com>
2025-07-06 13:52:06 +0100

    Add CLI for plotting YOLOE video annotations

--- src/camvidlog2/yoloe/cli.py
+++ src/camvidlog2/yoloe/cli.py
@@ -1,13 +1,15 @@
 from pathlib import Path
 from typing import Annotated
 
+import click
 import pandas as pd
 import typer
 
 from camvidlog2.common.data import load as data_load
-from camvidlog2.vid import generate_frames_cv2
+from camvidlog2.vid import generate_frames_cv2, get_video_stats, save_video
 from camvidlog2.yoloe.ai import generate_tracked_bboxes
 from camvidlog2.yoloe.data import create
+from camvidlog2.yoloe.plot import overlay_detections
 
 app = typer.Typer()
 
@@ -57,6 +59,31 @@ def load(
         # save array (new or existing) to disk
         existing_array.to_feather(db)
         print("saved file")
+
+
+@app.command()
+def show(
+    video: Path,
+    output: Path,
+    db: Annotated[Path, typer.Option()] = Path("tmp.feather"),
+):
+    # load existing array, if any
+    array = data_load(db)
+    if array is None:
+        raise click.FileError("database file must exist")
+
+    video = video.resolve()
+    array = array.loc[array.index.get_level_values("filename") == str(video)]
+    if array.shape[0] == 0:
+        raise ValueError("video must be in database")
+
+    stats = get_video_stats(video)
+    with save_video(output, stats=stats) as video_writer:
+        for frame in overlay_detections(
+            (f for _, f in generate_frames_cv2(video)),
+            array,
+        ):
+            video_writer.write(frame)
 
 
 if __name__ == "__main__":
---
+++ src/camvidlog2/yoloe/plot.py
@@ -0,0 +1,51 @@
+from typing import Generator, Iterator
+
+import numpy as np
+import pandas as pd
+import supervision as sv
+
+
+def overlay_detections(
+    frames: Iterator[np.ndarray], detections: pd.DataFrame
+) -> Generator[np.ndarray, None, None]:
+    """Overlay bounding boxes on frames."""
+
+    box_annotator = sv.BoxAnnotator()
+    label_annotator = sv.RichLabelAnnotator()
+    trace_annotator = sv.TraceAnnotator()
+
+    for i, frame in enumerate(frames):
+        # Select detections for the current frame via multilevel index
+        if i not in detections.index.get_level_values("frame_no"):
+            # no detections for this frame, yield the original frame
+            yield frame
+            continue
+        detections_frame = detections.xs(i, level="frame_no")
+        if detections_frame.empty:
+            yield frame
+            continue
+
+        detections_sv = sv.Detections(
+            xyxy=detections_frame.iloc[:, :4].to_numpy(),
+            confidence=detections_frame.iloc[:, 4].to_numpy(),
+            class_id=detections_frame.iloc[:, 5].cat.codes.astype(int).to_numpy(),
+            tracker_id=detections_frame.iloc[:, 6].astype(int).to_numpy(),
+        )
+
+        # Prepare labels for annotation
+        labels = [
+            f"{detections_frame.iloc[:, 5].cat.categories[cls]} {conf:.2f}"
+            for cls, conf in zip(detections_sv.class_id, detections_sv.confidence)
+        ]
+        # create a copy of the frame to annotate
+        scene = frame.copy()
+        # Annotate boxes
+        scene = box_annotator.annotate(scene=scene, detections=detections_sv)
+        # Annotate labels if available
+        if labels is not None:
+            scene = label_annotator.annotate(
+                scene=scene, detections=detections_sv, labels=labels
+            )
+        # Annotate traces if tracker IDs are available
+        scene = trace_annotator.annotate(scene=scene, detections=detections_sv)
+        yield scene
